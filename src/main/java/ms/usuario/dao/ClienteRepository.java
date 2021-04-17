d5d5cbea9edcd080056eda1b9aff5ce4273797e0
sfrigieri <santiagofrigieri@outlook.com>
2021-04-17 20:28:27 -0300

    + fix Cast Iterable to List

--- src/main/java/ms/usuario/dao/ClienteRepository.java
+++ src/main/java/ms/usuario/dao/ClienteRepository.java
@@ -1,7 +1,11 @@
 package ms.usuario.dao;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import frsf.isi.dan.InMemoryRepository;
@@ -9,25 +13,36 @@ import ms.usuario.domain.Cliente;
 
 @Repository
 public class ClienteRepository extends InMemoryRepository<Cliente> {
-	
+
 	@Override
 	public Integer getId(Cliente entity) {
 		return entity.getId();
 	}
-	
+
 	@Override
 	public void setId(Cliente entity, Integer id) {
 		entity. setId(id);
 	}
-	
+
 	public Optional<Cliente> findByCuit(String cuit) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Cliente> c =  this.findAll()
+				.stream()
+				.filter(unCli -> unCli.getCuit().equals(cuit))
+				.findFirst();
+		return c;
 	}
 
 	public Optional<Cliente> findByRazonSocial(Optional<String> razonSocial) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Cliente> c =  this.findAll()
+				.stream()
+				.filter(unCli -> unCli.getRazonSocial().equals(razonSocial.get()))
+				.findFirst();
+		return c;
 	}
-	
+
+	public List<Cliente> findAll(){
+		return StreamSupport.stream(super.findAll().spliterator(), false)
+				.collect(Collectors.toList());
+	}
+
 }
