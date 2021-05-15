5bcc235a35dd2139ff5707c3c2a098176937156e
sfrigieri <santiagofrigieri@outlook.com>
2021-05-15 00:31:55 -0300

    + spring.datasource.initialization-mode=always > data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO usr_tipo_obra (descripcion) VALUES ('Edificio');
+INSERT INTO usr_tipo_obra (descripcion) VALUES ('Vivienda');
+INSERT INTO usr_tipo_obra (descripcion) VALUES ('Vial');
+
+INSERT INTO usr_tipo_usuario (tipo) VALUES ('Cliente');
+INSERT INTO usr_tipo_usuario (tipo) VALUES ('Empleado');
